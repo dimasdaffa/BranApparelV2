5d09a592587135cbc3807f8c55a4cfefa9bd8ebe
dimasdaffa <dimas.de2004@gmail.com>
2025-02-24 10:00:21 +0700

    tampilkan data yg ada di homepage dari backend crud

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CompanyStatistic;
+use App\Models\HeroSection;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use App\Models\Product;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index()
     {
-        return view('front.index');
+        $statistics = CompanyStatistic::take(4)->get();
+        $principles = OurPrinciple::take(4)->get();
+        $products = Product::take(4)->get();
+        $teams = OurTeam::take(3)->get();
+        $testimonials = Testimonial::take(5)->get();
+        $hero_section = HeroSection::orderByDesc('id')->take(1)->get();
+        return view('front.index',compact('statistics','principles','products','teams','testimonials','hero_section'));
     }
 }
--- public/js/modal-video.js
+++ public/js/modal-video.js
@@ -1,5 +1,6 @@
 let videoFrame = document.getElementById('videoFrame');
 const targetEl = document.getElementById('video-modal');
+const pathVideo = document.getElementById('path_video').value;
 
 const options = {
     placement: 'bottom-right',
@@ -7,10 +8,10 @@ const options = {
     backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
     closable: true,
     onHide: () => {
-        videoFrame.removeAttribute('src'); 
+        videoFrame.removeAttribute('src');
     },
     onShow: () => {
-        videoFrame.src = 'https://www.youtube.com/embed/ZWZfwmObdvc';
+        videoFrame.src = 'https://www.youtube.com/embed/'+pathVideo;
     },
 };
 
